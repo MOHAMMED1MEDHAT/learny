101d79dd60c1308c9e4353c3f972d3251bc38247
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-05-24 01:55:07 +0300

    Update profire response structure 0.5

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -8,7 +8,7 @@ const jwtSCRT = config.get("env_var.jwtScreteKey");
 //get by user id
 const getUser = async (req, res) => {
     try {
-        const token = req.header("Cookie");
+        const token = req.header("x-auth-token");
         // const token = req.params.id;
         console.log("token", token);
         const { userId } = jwt.verify(token, jwtSCRT);
