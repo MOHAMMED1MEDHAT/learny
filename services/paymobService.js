ed63781d6f252f6d697476a921f746cfb466049e
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-06-25 11:40:45 +0300

    Added paymob payment feature 0.3

--- services/paymobService.js
+++ services/paymobService.js
@@ -2,75 +2,76 @@ require("dotenv").config({ path: __dirname + "../.env" });
 
 exports.getPaymobPaymentLink = async ({ user, cost }) => {
     try {
-        //A.4.1: first paymob api request
-        const paymobFirstResponse = await fetch(
+        const axios = require("axios");
+
+        // A.4.1: First paymob API request
+        const paymobFirstResponse = await axios.post(
             "https://accept.paymob.com/api/auth/tokens",
             {
-                method: "POST",
+                api_key: process.env.PAYMOB_API_KEY,
+            },
+            {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                    api_key: process.env.PAYMOB_API_KEY,
-                }),
             }
         );
-        const { token } = await paymobFirstResponse.json();
-        //A.4.2: sconde paymob api request
-        const paymobScondeRequest = await fetch(
+        const { token } = paymobFirstResponse.data;
+
+        // A.4.2: Second paymob API request
+        const paymobSecondRequest = await axios.post(
             "https://accept.paymob.com/api/ecommerce/orders",
             {
-                method: "POST",
+                auth_token: token,
+                delivery_needed: "false",
+                amount_cents: cost.toString(),
+                currency: "EGP",
+                items: [],
+            },
+            {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                    auth_token: token,
-                    delivery_needed: "false",
-                    amount_cents: cost.toString(),
-                    currency: "EGP",
-                    items: [],
-                }),
             }
         );
-        const { id } = await paymobScondeRequest.json();
-        //A.4.3: third paymob api request
-        const paymobThirdResponse = await fetch(
+        const { id } = paymobSecondRequest.data;
+
+        // A.4.3: Third paymob API request
+        const paymobThirdResponse = await axios.post(
             "https://accept.paymob.com/api/acceptance/payment_keys",
             {
-                method: "POST",
+                auth_token: token,
+                amount_cents: cost.toString(),
+                expiration: 3600,
+                order_id: id,
+                billing_data: {
+                    apartment: "NA",
+                    email: user.email,
+                    floor: "NA",
+                    first_name: user.name,
+                    street: "NA",
+                    building: "NA",
+                    phone_number: user.phone,
+                    shipping_method: "NA",
+                    postal_code: "NA",
+                    city: "NA",
+                    country: "NA",
+                    last_name: user.name,
+                    state: "NA",
+                },
+                currency: "EGP",
+                integration_id: process.env.PAYMOB_PAYMENT_INTEGRATION_ID,
+            },
+            {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                    auth_token: token,
-                    amount_cents: cost.toString(),
-                    expiration: 3600,
-                    order_id: id,
-                    billing_data: {
-                        apartment: "NA",
-                        email: user.email,
-                        floor: "NA",
-                        first_name: user.name,
-                        street: "NA",
-                        building: "NA",
-                        phone_number: user.phone,
-                        shipping_method: "NA",
-                        postal_code: "NA",
-                        city: "NA",
-                        country: "NA",
-                        last_name: user.name,
-                        state: "NA",
-                    },
-                    currency: "EGP",
-                    integration_id: process.env.PAYMOB_PAYMENT_INTEGRATION_ID,
-                }),
             }
         );
-        const secondToken = await paymobThirdResponse.json();
+        const secondToken = paymobThirdResponse.data.token;
 
         return {
-            iframeLink: `https://accept.paymob.com/api/acceptance/iframes/763553?payment_token=${secondToken.token}`,
+            iframeLink: `https://accept.paymob.com/api/acceptance/iframes/763553?payment_token=${secondToken}`,
             orderId: id,
         };
     } catch (error) {
