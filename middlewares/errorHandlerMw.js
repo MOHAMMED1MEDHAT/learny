eddc7b9b98a42eee6efc656f4e9608635cbfd8dc
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-07 10:28:23 +0300

    Error Handling Refactor 0.5

--- middlewares/errorHandlerMw.js
+++ middlewares/errorHandlerMw.js
@@ -1,4 +1,4 @@
-const AppError = require("./../util/appError");
+const AppError = require("./../util/AppError");
 
 const handleCastErrorDB = (err) => {
     const message = `Invalid ${err.path}: ${err.value}`;
